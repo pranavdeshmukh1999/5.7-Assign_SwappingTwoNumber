bada622d46304db63870c5b817bc7d0f25314b26
pranavdeshmukh1999 <pranavdeshmukh182@gmail.com>
2022-09-17 17:22:26 +0530

    Added: Swapping Two Number

---
+++ src/com/blz/swapassign/SwapTwoNumber.java
@@ -0,0 +1,37 @@
+package com.blz.swapassign;
+
+import java.util.Scanner;
+
+public class SwapTwoNumber {
+
+	public static void main(String[] args) {
+		int num = 0;
+		Scanner swap = new Scanner(System.in);
+		System.out.println("Enter First Number :");
+
+		int x = swap.nextInt();
+		System.out.println("Enter Second Number :");
+
+		int y = swap.nextInt();
+		System.out.println("Original Number is = " + x + "" + y);
+		num = x;
+		x = y;
+		y = num;
+		System.out.println("Number After Swaping = " + x + "" + y);
+
+		System.out.println("THIS IS SWAPING PROGRAM......THANK YOU");
+	}
+
+//	Scanner swap = new Scanner(System.in);
+//	System.out.println("Enter First Number:");
+//	int x = swap.nextInt();
+//	System.out.println("Enter Second Number");
+//	int y = swap.nextInt();
+//	System.out.println("Original Number is = " + x + "" + y);
+//	num = x;
+//	x = y;
+//	y = num;
+//	System.out.println("Number After Swaping = " + x + "" + y);
+//}
+
+}
